25a65234462717f0a3142327d0a73173644c0c34
Rafael <rcarneiro782@gmail.com>
2022-04-27 19:10:42 -0300

    part -3

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 let order = [];
 let clickedOrder =[];
 let score=0;
+let time=0;
 
 
 // 0= verde, 1 = vermelho, 2= amarelo e 3 = azul
@@ -20,6 +21,18 @@ let shuffOrder = () =>{
      clickedOrder=[];
 
      for(let i in order){
-         
+        let elementColor = createColorElement(order[i]);
+        lightColor(elementColor, Number(i) + 1);
      }
+}
+
+
+let lightColor = (element, number) => {
+     number = number * 500;
+     setTimeout(() => {
+     element.classList.add('selected')
+     }, number - 250);
+setTimeou(() => {
+     element.classList.remove('selected');
+});
 }
